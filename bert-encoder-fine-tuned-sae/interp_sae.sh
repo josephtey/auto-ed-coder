62aed81a19fdd92859d4d5c65e0a5793a3d6bfeb
Joseph Tey <jjttey@gmail.com>
2024-07-23 22:53:19 -0700

    used the education dataset instead

--- bert-encoder-fine-tuned-sae/interp_sae.sh
+++ bert-encoder-fine-tuned-sae/interp_sae.sh
@@ -1,1 +1,5 @@
-python interp_sae.py --sentences_file "pickled_data/20240723_211713_sentences.npy" --embeddings_file "pickled_data/20240723_211713_embeddings.npy" --sae_base_path sae/20240723_214753 --features_base_path features
+python interp_sae.py \
+    --sentences_file "pickled_data/20240723_211713_sentences.npy" \
+    --embeddings_file "pickled_data/20240723_211713_embeddings.npy" \
+    --sae_base_path sae/20240723_214753 \
+    --features_base_path features
